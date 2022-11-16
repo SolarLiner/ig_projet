54d8b82a2847917eb1962abb46f112502026ee77
Nathan Graule <solarliner@gmail.com>
2022-11-16 23:24:57 +0100

    fix: segfault on drawing dynamically-updated arrays

--- src/shell/gl/resource/buffers/UniformBuffer.h
+++ src/shell/gl/resource/buffers/UniformBuffer.h
@@ -5,11 +5,11 @@
 #ifndef IG_PROJET_UNIFORMBUFFER_H
 #define IG_PROJET_UNIFORMBUFFER_H
 
-#endif//IG_PROJET_UNIFORMBUFFER_H
-
 #include "Buffer.h"
 
 namespace shell::gl::resource {
     template <typename T, BufferDraw Draw = Static>
     using UniformBuffer = Buffer<T, Uniform, Draw>;
 }
+
+#endif//IG_PROJET_UNIFORMBUFFER_H
